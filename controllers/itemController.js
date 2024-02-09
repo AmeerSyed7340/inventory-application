30c0659031201a1b7d69537afc9f53a3876afd79
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-02-09 12:03:52 -0500

    Synchronised index view with itemController

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,10 +1,21 @@
 const Item = require("../models/item");
+const Category = require("../models/category");
 
 const asyncHandler = require("express-async-handler")
 
 //index
 exports.index = asyncHandler(async (req, res, next) => {
-    res.render('index', {
+    const [
+        numItems,
+        numCategories
+    ] = await Promise.all([
+        Item.countDocuments({}).exec(),
+        Category.countDocuments({}).exec(),
+    ]);
+
+    res.render("index", {
         title: "Inventory Application",
+        item_count: numItems,
+        category_count: numCategories
     });
 });
