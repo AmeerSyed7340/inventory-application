f8c5dbb3f60f3e77f5f210aead8ac10f5df8b607
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-02-09 12:21:31 -0500

    Updated category list to display all categories

--- routes/catalog.js
+++ routes/catalog.js
@@ -8,4 +8,7 @@ const item_controller = require("../controllers/itemController");
 //GET catalog homepage
 router.get("/", item_controller.index);
 
+//GET all categeories
+router.get('/categories', category_controller.category_list)
+
 module.exports = router;
